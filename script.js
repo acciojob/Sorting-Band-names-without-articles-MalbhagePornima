f487939ce8b9d8585cf2a9904def361700f84e25
MalbhagePornima <142093558+MalbhagePornima@users.noreply.github.com>
2023-10-17 15:03:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
-
+let withoutArticles=[];
+let map={};
+for(let i=0;i<arr.length;i++)
+	{
+		let str=arr[i];
+		str=str.replace(/|ban\b|\bthe\b|ba\b/gi, "");
+		str=str.replace(/ +/gi, " ");
+		str=str.trim();
+		map[str]=arr[i];
+		withoutArticles.push(str);
+	}
+for(let i=0;i<withoutArticles;i++)
+	{
+		console.log(map[withoutArticles[i] ]);
+	}
